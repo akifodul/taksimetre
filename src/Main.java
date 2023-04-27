c2749c04817076aeaccf83aaad75d65c2aa8b8bb
[akif ödül] <[akifodul@hotmail.com]>
2023-04-27 06:16:22 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    int km;
+    double perKm = 2.20, total, startPrice = 10;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Mesafeyi KM cinsinden girin: ");
+    km = input.nextInt();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+    total = (km * perKm);
+    total += startPrice;    // total = total + startPrice is same thing.
+
+    total = (total < 20) ? 20 : total;  // total 20den küçükse 20, değilse totali yazdır komutu.
+    System.out.println("Toplam Tutar: " + total);
+
     }
 }
